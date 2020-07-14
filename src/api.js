a3cdfb214fb1d99467a0eb35b27258b854feb29c
Eclipse <lpskirito7@outlook.com>
2020-07-14 12:23:54 -0400

    second commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export default[
+ getItems()
+]
+
+
+let BASE_URL = 'https://thinkful-list-api.herokuapp.com/michaelhoover';
+
+function getItems(){
+ return Promise.resolve('A successful response!');
+}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
+import api from './api';
 
 const main = function () {
-  console.log('hello world')
+  api.getItems()
+  .then(res => console.log(res));
+
+console.log(api.BASE_URL);
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
